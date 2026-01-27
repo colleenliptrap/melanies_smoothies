a911e8a6c9f6b5b5f42fd1bceacfa525e59e1aa1
colleenliptrap <colleenliptrap@gmail.com>
2026-01-27 14:44:29 -0500

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+streamlit.title('My Parents New Healthy Diner')
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app
